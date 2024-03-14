0d9457692440a29b27c7512c0c890e2e0728691f
Jason19951003 <Jason19951003@gmail>
2024-03-14 14:00:23 +0800

    123456

--- src/com/test/jason/Test.java
+++ src/com/test/jason/Test.java
@@ -3,8 +3,23 @@ package com.test.jason;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HelloWorld!");
-	}
+		try {
+			int guess = Integer.parseInt(args[0]);
+			if (guess < 1 || guess > 5) {
+				System.out.println("程式用法 java GuessingGame [數字1-5]");
+			} else {
+				int random = (int) (Math.random() * 5) + 1;
 
+				if (random == guess)
+					System.out.println("恭喜猜對了!");
+				else
+					System.out.println("猜錯了,答案是" + random);
+			}
+		} catch (Exception e) {
+			if (args[0].equals("help"))
+				System.out.println("程式用法 java GuessingGame [數字1-5]");
+			else
+				System.out.println("輸入內容須為正整數");
+		}
+	}
 }
