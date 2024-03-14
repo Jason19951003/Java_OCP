8b8a707d35f59ce06aa66b3261f25c84e7e1bf16
Jason19951003 <Jason19951003@gmail>
2024-03-14 14:09:28 +0800

    刪除註解

--- src/com/test/jason/GuessingGame.java
+++ src/com/test/jason/GuessingGame.java
@@ -1,18 +1,7 @@
 package com.test.jason;
 
 public class GuessingGame {
-	public static void main(String[] args) {
-		/*
-		 * try { int guess = Integer.parseInt(args[0]); if (guess<1 || guess>5) {
-		 * System.out.println("程式用法 java GuessingGame [數字1-5]"); } else { int random =
-		 * (int)(Math.random()*5) + 1;
-		 * 
-		 * if (random == guess) System.out.println("恭喜猜對了!"); else
-		 * System.out.println("猜錯了,答案是" + random); } } catch(Exception e) { if
-		 * (args[0].equals("help"))
-		 * System.out.println("程式用法 java GuessingGame [數字1-5]"); else
-		 * System.out.println("輸入內容須為正整數"); }
-		 */
+	public static void main(String[] args) {		
 		if (args.length == 0 || args[0].equals("help")) {
 			System.out.println("程式用法 java GuessingGame [數字1-5]");
 		} else if (args[0].matches("[0-9]+")) {
